7bef9d8dad87bd6c3c8116e5db5e3652f5782a3f
Nate Finch <finchproservices@gmail.com>
2016-08-16 14:25:04 +0900

    added post navigation for single posts

--- single.php
+++ single.php
@@ -20,25 +20,36 @@ get_template_part( 'template-parts/content-heros' );
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_format() );
-
-
-			?>
-			<hr/>
 			<?php
+			while ( have_posts() ) : the_post();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				get_template_part( 'template-parts/content', get_post_format() );
 
-		endwhile; // End of the loop.
-		?>
+
+				?>
+				<hr/>
+				<?php
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
 
 		</main><!-- #main -->
+
+		<?php
+		if ( is_singular( 'post' ) ) {
+			// Previous/next post navigation.
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'meg-n-boots' ) . '</span> ',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'meg-n-boots' ) . '</span> '
+			) );
+		}
+		?>
+
 	</div><!-- #primary -->
 
 
